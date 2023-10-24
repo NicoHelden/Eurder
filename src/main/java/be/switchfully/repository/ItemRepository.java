7bd1c15b51f741ec84821a21f41882601dfd5b49
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-10-24 21:03:41 +0200

    Second commit

--- src/main/java/be/switchfully/repository/ItemRepository.java
+++ src/main/java/be/switchfully/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package be.switchfully.repository;
 import be.switchfully.domain.item.Item;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static be.switchfully.database.EurderDb.itemsById;
 
 @ApplicationScoped
@@ -11,4 +14,7 @@ public class ItemRepository {
         itemsById.put(item.getId(), item);
     }
 
+    public Set<Item> getAll() {
+        return new HashSet<>(itemsById.values());
+    }
 }
